127af64b5684988ea907d7c3867de39d8b89195e
Leela_Krishna <leela_krishna@dell.com>
2024-09-02 13:27:34 +0530

    code updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import PlayercontextProvider from "./context/Playercontext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <PlayercontextProvider>
+        <App />
+      </PlayercontextProvider>
     </BrowserRouter>
   </StrictMode>
 );
